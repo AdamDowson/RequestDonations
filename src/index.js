e4c7a0663b15edb32c607d6be9adce1ed420a0cc
AdmREQ <adamdowson@googlemail.com>
2018-08-29 08:22:51 +0100

    Added makerDAO demo and tidied up redundant code in index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import config from './config.json';
-var ejs = require('ejs');
-var https = require('https');
-var fs = require('fs');
-var path = require('path');
 
 let app = express();
 
@@ -16,19 +12,6 @@ app.get('/thank-you', function (req, res) {
 		{ txid: req.query.txid, owed: req.query.owed, currency: req.query.currency, redirect: req.query.redirect, fiat: req.query.fiat })
 });
 
-// app.get('/demo', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/../demo/index.html'));
-// });
-
-// Server setup
-
-// var options = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/sign.wooreq.com/privkey.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/sign.wooreq.com/cert.pem'),
-//   ca: fs.readFileSync('/etc/letsencrypt/live/sign.wooreq.com/chain.pem')
-// };
-// app.server = https.createServer(options, app);
-
 app.server = http.createServer(app);
 
 app.use(bodyParser.json());
@@ -36,6 +19,7 @@ app.set('view engine', 'ejs');
 
 app.use('/demo', express.static('demo'));
 app.use('/demo2', express.static('demo2'));
+app.use('/makerdemo', express.static('demo-maker'));
 app.use(express.static('public'));
 
 
